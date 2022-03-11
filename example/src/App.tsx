2c7b7bc7bed753de252dee34b1e34db2fe53e6fa
phellipe5k <luizinhophe@hotmail.com>
2022-03-10 23:35:13 -0300

    Starting WEB3UI Lib 🎉🎉

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'web3ui'
+import 'web3ui/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
